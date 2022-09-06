5717ba06306b80d5c84ecf4cf145939bb160c030
airman5573 <rpf5573@kakao.com>
2022-09-06 21:42:36 +0900

    chore: commit message앞의 이모지를 삭제한다

--- .cz-config.js
+++ .cz-config.js
@@ -1,41 +1,41 @@
 module.exports = {
   types: [
-    { value: ':sparkles: feat', name: '✨ feat:\tAdding a new feature' },
-    { value: ':bug: fix', name: '🐛 fix:\tFixing a bug' },
-    { value: ':memo: docs', name: '📝 docs:\tAdd or update documentation' },
+    { value: "feat", name: "✨ feat:\tAdding a new feature" },
+    { value: "fix", name: "🐛 fix:\tFixing a bug" },
+    { value: "docs", name: "📝 docs:\tAdd or update documentation" },
     {
-      value: ':lipstick: style',
-      name: '💄 style:\tAdd or update styles, ui or ux',
+      value: "style",
+      name: "style:\tAdd or update styles, ui or ux",
     },
     {
-      value: ':recycle: refactor',
-      name: '♻️ refactor:\tCode change that neither fixes a bug nor adds a feature',
+      value: "refactor",
+      name: "refactor:\tCode change that neither fixes a bug nor adds a feature",
     },
     {
-      value: ':pencil2: typo',
-      name: '✏️ typo:\tFix typo',
+      value: "typo",
+      name: "typo:\tFix typo",
     },
     {
-      value: ':zap: perf',
-      name: '⚡️ perf:\tCode change that improves performance',
+      value: "perf",
+      name: "perf:\tCode change that improves performance",
     },
     {
-      value: ':white_check_mark: test',
-      name: '✅ test:\tAdding tests cases',
+      value: "test",
+      name: "test:\tAdding tests cases",
     },
     {
-      value: ':truck: chore',
-      name: '🚚 chore:\tChanges to the build process or auxiliary tools\n\t\tand libraries such as documentation generation',
+      value: "chore",
+      name: "chore:\tChanges to the build process or auxiliary tools\n\t\tand libraries such as documentation generation",
     },
-    { value: ':rewind: revert', name: '⏪️ revert:\tRevert to a commit' },
-    { value: ':construction: wip', name: '🚧 wip:\tWork in progress' },
+    { value: "revert", name: "⏪️ revert:\tRevert to a commit" },
+    { value: "wip", name: "🚧 wip:\tWork in progress" },
     {
-      value: ':construction_worker: build',
-      name: '👷 build:\tAdd or update regards to build process',
+      value: "build",
+      name: "build:\tAdd or update regards to build process",
     },
     {
-      value: ':green_heart: ci',
-      name: '💚 ci:\tAdd or update regards to build process',
+      value: "ci",
+      name: "ci:\tAdd or update regards to build process",
     },
   ],
 
@@ -49,7 +49,7 @@ module.exports = {
   // },
 
   allowCustomScopes: true,
-  allowBreakingChanges: ['feat', 'fix'],
+  allowBreakingChanges: ["feat", "fix"],
   // skip any questions you want
   subjectLimit: 100,
 };
