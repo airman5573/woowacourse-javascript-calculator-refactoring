39d28afab1503dc163040f4be42498bd6033cd4d
airman5573 <rpf5573@kakao.com>
2022-09-13 19:44:47 +0900

    refactor: 상수 객체의 property도 대문자로 한다

--- src/constants.js
+++ src/constants.js
@@ -10,8 +10,8 @@ export const INITIAL_NUMBER = 0;
 export const MAX_DIGIT_SIZE = 3;
 
 export const ERROR_MESSAGES = {
-  underMaxDigitSize: "세자리수 까지만 입력 가능합니다",
-  operationBetweenNumber: "연산자는 숫자 사이에 입력해 주세요",
-  onlyTwoOperlands: "두개의 숫자만 연산이 가능합니다",
-  divideWithPositiveNumber: "양의 정수로만 나눌 수 있습니다",
+  UNDER_MAX_DIGIT_SIZE: "세자리수 까지만 입력 가능합니다",
+  OPERATION_BETWEEN_NUMBER: "연산자는 숫자 사이에 입력해 주세요",
+  ONLY_TWO_OPERAND: "두개의 숫자만 연산이 가능합니다",
+  DIVIDE_WITH_POSITIVE_NUMBER: "양의 정수로만 나눌 수 있습니다",
 };
